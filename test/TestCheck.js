550c872a475ae2ff86d054a26a2917f614e705a4
“Richard <rich@Richards-MacBook-Air.local>
2015-04-23 10:56:13 +0100

    Added first Mocha test

---
+++ test/TestCheck.js
@@ -0,0 +1,8 @@
+var assert = require("assert"); // node.js core module
+
+
+    describe('Check testing framework is present', function(){
+        it('should pass', function(){
+            assert.equal(true,true); // 4 is not present in this array so indexOf returns -1
+        })
+    });
